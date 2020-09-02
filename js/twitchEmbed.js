673b632d7e69951f333652656564855d23b79405
patmakes <58449753+patmakes@users.noreply.github.com>
2020-09-02 19:18:20 -0400

    good luck

--- js/twitchEmbed.js
+++ js/twitchEmbed.js
@@ -3,7 +3,7 @@ var embed = new Twitch.Embed("twitch-embed", {
     height: 480,
     channel: "patmakesmusic",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    parent: ["https://pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html"]
+    parent: ["https://pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html"],
   });
 
   embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
