ab41a25f9cad0678df45f62387344c1871cbada8
patmakes <58449753+patmakes@users.noreply.github.com>
2020-09-02 19:08:43 -0400

    try twitch embed

---
+++ js/twitchEmbed.js
@@ -0,0 +1,11 @@
+var embed = new Twitch.Embed("twitch-embed", {
+    width: 854,
+    height: 480,
+    channel: "patmakesmusic",
+    // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+    parent: "https://pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html"
+  });
+
+  embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+      console.log('video ready')
+  })
