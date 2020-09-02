e19f501b585606e75e88fcb39bf94c267651d7ab
patmakes <58449753+patmakes@users.noreply.github.com>
2020-09-02 19:31:45 -0400

    try diff method

--- js/twitchEmbed.js
+++ js/twitchEmbed.js
@@ -1,11 +1,21 @@
-var embed = new Twitch.Embed("twitch-embed", {
+// var embed = new Twitch.Embed("twitch-embed", {
+//     width: 854,
+//     height: 480,
+//     channel: "patmakesmusic",
+//     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+//     parent: ["pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html"]
+//   });
+
+//   embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+//       console.log('video ready')
+//   });
+
+  var options = {
     width: 854,
     height: 480,
     channel: "patmakesmusic",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    parent: ["pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html"]
-  });
-
-  embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
-      console.log('video ready')
-  })
+    // parent: ["embed.example.com", "othersite.example.com"]
+  };
+  var player = new Twitch.Player("twitch-embed", options);
+  player.setVolume(0.5);
