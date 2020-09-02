471647da64deff0076eec3a92e60558bf632708d
patmakes <58449753+patmakes@users.noreply.github.com>
2020-09-02 19:49:11 -0400

    tried adding String[]

--- js/twitchEmbed.js
+++ js/twitchEmbed.js
@@ -2,12 +2,18 @@ var embed = new Twitch.Embed("twitch-embed", {
     width: 854,
     height: 480,
     channel: "patmakesmusic",
+    layout: "video",
+    autoplay: false,
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
-    parent: ["pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html",]
+    parent: String["pages.git.generalassemb.ly/patmakes/patMakesMusic/index.html"]
   });
 
   embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
       console.log('video ready')
+
+      var player = embed.getPlayer();
+
+        player.play();
   });
 
 //   var options = {
