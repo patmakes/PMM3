97d2152e36be476003c42f4f87c19b4ba489ea97
patmakes <patrickmerner02@gmail.com>
2020-12-24 04:07:57 -0500

    minor CSS and support for Print 2 PDF

---
+++ js/print2PDF.js
@@ -0,0 +1,35 @@
+let button = document.getElementById("PrintToPDF");
+let body = document.querySelector("body");
+// let ignore = document.getElementById("ignore");
+let header = document.querySelector("header");
+let footer = document.querySelector("footer")
+
+function cleanUp() {
+
+header.classList.add("ignore");
+footer.classList.add("ignore");
+button.classList.add("ignore");
+console.log("cleanup");
+
+};
+
+window.addEventListener("beforeprint", cleanUp);
+
+
+function print2PDF() {
+
+window.print();
+
+};
+
+button.addEventListener("click", print2PDF);
+
+function restore() {
+
+  header.classList.remove("ignore");
+  footer.classList.remove("ignore");
+  button.classList.remove("ignore");
+
+};
+
+window.addEventListener("afterprint", restore);
